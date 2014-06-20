3cd68d05d1215247a7dd348801e86f382b766fd5
wei.xu <xuwei5@huawei.com>
2014-06-20 13:05:14 +0800

    update autotest to apply 0004 patch

--- autotest-server-install.sh
+++ autotest-server-install.sh
@@ -2,7 +2,9 @@
 
 wget -c https://raw.githubusercontent.com/autotest/autotest/master/contrib/install-autotest-server.sh
 wget -c https://raw.githubusercontent.com/joyxu/autotest-patches/master/0003-apply-the-patch-from-joyxu-github.patch
+wget -c https://raw.githubusercontent.com/joyxu/autotest-patches/master/0004-update-install-autotest-server.sh-to-downdload-divided-gwt-package.patch
 patch -N -p2 < 0003-apply-the-patch-from-joyxu-github.patch
+patch -N -p2 < 0004-update-install-autotest-server.sh-to-downdload-divided-gwt-package.patch
 chmod +x install-autotest-server.sh
 ./install-autotest-server.sh -u 123456789 -d 123456789
 
